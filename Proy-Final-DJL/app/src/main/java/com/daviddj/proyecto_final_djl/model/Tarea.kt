78cea4132776124b9cb05aa8277d8ff0e5777643
David09 <s20120178@alumnos.itsur.edu.mx>
2023-10-02 22:42:02 -0600

    Creación del modelo Tarea y package model

--- Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/model/Tarea.kt
+++ Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/model/Tarea.kt
@@ -1,9 +1,10 @@
-package com.daviddj.proyecto_final_djl
+package com.daviddj.proyecto_final_djl.model
 
 import java.util.Date
-data class Nota(
+data class Tarea (
     var name: String,
     var description: String,
     val fecha: Date,
+    var isComplete: Boolean,
     var contenido: String
 )
