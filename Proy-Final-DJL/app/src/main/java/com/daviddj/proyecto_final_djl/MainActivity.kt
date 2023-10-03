da542b8b8dc8797ec05d03d5735e728c131e705b
David09 <s20120178@alumnos.itsur.edu.mx>
2023-10-02 23:17:55 -0600

    Creación de la TopBar de la pantalla principal

--- Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/MainActivity.kt
+++ Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/MainActivity.kt
@@ -3,13 +3,26 @@ package com.daviddj.proyecto_final_djl
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.daviddj.proyecto_final_djl.ui.theme.ProyectoFinalDJLTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,17 +35,47 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AppTopBar()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun AppTopBar(modifier: Modifier =  Modifier){
+    CenterAlignedTopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Button(onClick = { /*TODO*/ }) {
+                    Image(
+                        modifier = Modifier
+                            .size(40.dp)
+                            .padding(4.dp),
+                        painter = painterResource(R.drawable.notas),
+                        contentDescription = null
+                    )
+                    Text(
+                        text = stringResource(R.string.notas),
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+                Spacer(modifier = Modifier.width(10.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Image(
+                        modifier = Modifier
+                            .size(40.dp)
+                            .padding(4.dp),
+                        painter = painterResource(R.drawable.tareas),
+                        contentDescription = null
+                    )
+                    Text(text = stringResource(R.string.tareas),
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+        },
         modifier = modifier
     )
 }
@@ -40,7 +83,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ProyectoFinalDJLTheme {
-        Greeting("Android")
+    ProyectoFinalDJLTheme(useDarkTheme=false) {
+        AppTopBar()
     }
 }
