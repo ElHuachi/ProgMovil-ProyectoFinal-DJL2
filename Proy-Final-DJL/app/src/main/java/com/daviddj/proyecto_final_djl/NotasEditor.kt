9deed3c537db82ab492c6b4fecbf4aa5e619dde2
Juan José Arreola Gonzalez <juanjosearreola99@gmail.com>
2023-10-03 21:02:40 -0600

    NotasEditor Terminada

---
+++ Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/NotasEditor.kt
@@ -0,0 +1,95 @@
+package com.daviddj.proyecto_final_djl
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.daviddj.proyecto_final_djl.model.Nota
+
+@Composable
+fun EditorNotas(nota : Nota, modifier: Modifier = Modifier){
+    Column (
+        modifier= Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row (horizontalArrangement = Arrangement.Start) {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Filled.KeyboardArrowLeft, contentDescription = "Regresar",
+                    modifier = Modifier
+                        .size(32.dp)
+                        .padding(0.dp)
+                )
+            }
+        }
+        Text(
+            text = nota.name,
+            style = MaterialTheme.typography.headlineLarge
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = nota.name,
+            style = MaterialTheme.typography.headlineLarge
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = nota.fecha.toString(),
+            style = MaterialTheme.typography.bodySmall
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = nota.description,
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(onClick = { /*TODO*/ }) {
+                Image(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(4.dp),
+                    painter = painterResource(R.drawable.camara_fotografica),
+                    contentDescription = null
+                )
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Image(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(4.dp),
+                    painter = painterResource(R.drawable.carpeta),
+                    contentDescription = null
+                )
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Image(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(4.dp),
+                    painter = painterResource(R.drawable.microfono),
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
