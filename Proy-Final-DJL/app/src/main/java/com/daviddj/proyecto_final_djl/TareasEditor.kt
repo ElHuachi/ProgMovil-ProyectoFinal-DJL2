97147a403466ec5ac481b38349f378e24580e8b4
Juan José Arreola Gonzalez <juanjosearreola99@gmail.com>
2023-10-03 21:35:59 -0600

    TareasEditor Listo

---
+++ Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/TareasEditor.kt
@@ -0,0 +1,128 @@
+package com.daviddj.proyecto_final_djl
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.daviddj.proyecto_final_djl.model.Tarea
+
+@Composable
+fun EditorTareas(tarea: Tarea,modifier: Modifier = Modifier) {
+    var checkedState = remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Filled.KeyboardArrowLeft,
+                    contentDescription = "Regresar",
+                    modifier = Modifier
+                        .size(32.dp)
+                        .padding(0.dp)
+                )
+            }
+            Spacer(modifier = Modifier.width(40.dp))
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Filled.KeyboardArrowLeft,
+                    contentDescription = "Recordatorios",
+                    modifier = Modifier
+                        .size(32.dp)
+                        .padding(0.dp)
+                )
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Filled.DateRange,
+                    contentDescription = "Recordatorios",
+                    modifier = Modifier
+                        .size(32.dp)
+                        .padding(0.dp)
+                )
+            }
+            CheckboxWithText(
+                text = "Completado",
+                isChecked = checkedState,
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(end = 8.dp)
+            ) { var isChecked = it}
+        }
+        Text(
+            text = tarea.name,
+            style = MaterialTheme.typography.headlineLarge
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = tarea.fecha.toString(),
+            style = MaterialTheme.typography.bodySmall
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = tarea.description,
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Button(onClick = { /*TODO*/ }) {
+                Image(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(4.dp),
+                    painter = painterResource(R.drawable.camara_fotografica),
+                    contentDescription = null
+                )
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Image(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(4.dp),
+                    painter = painterResource(R.drawable.carpeta),
+                    contentDescription = null
+                )
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Image(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(4.dp),
+                    painter = painterResource(R.drawable.microfono),
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
