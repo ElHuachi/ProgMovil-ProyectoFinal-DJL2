b7e4a7347925c41ab539a23955c4af764c63ca8d
Manuel <perezmane07@gmail.com>
2023-10-30 22:22:51 -0600

    Creación de las funciones agregar, eliminar y actualizar tareas.

--- Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/data/NotaDAO.kt
+++ Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/data/NotaDAO.kt
@@ -1,4 +1,24 @@
 package com.daviddj.proyecto_final_djl.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.daviddj.proyecto_final_djl.model.Nota
+
+@Dao
 interface NotaDAO {
+    /*
+    @Query("SELECT * FROM notas")
+    fun getAll(): List<Nota>
+    */
+    @Insert
+    fun insertAll(nota: Nota)
+
+    @Delete
+    fun delete(nota: Nota)
+
+    @Update
+    fun update(nota: Nota)
 }
--- Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/data/TareaDAO.kt
+++ Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/data/TareaDAO.kt
@@ -1,4 +1,24 @@
 package com.daviddj.proyecto_final_djl.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.daviddj.proyecto_final_djl.model.Tarea
+
+@Dao
 interface TareaDAO {
+    /*
+    @Query("SELECT * FROM tareas")
+    fun getAll(): List<Tareas>
+    */
+    @Insert
+    fun insertAll(tarea: Tarea)
+
+    @Delete
+    fun delete(tarea: Tarea)
+
+    @Update
+    fun update(tarea: Tarea)
 }
