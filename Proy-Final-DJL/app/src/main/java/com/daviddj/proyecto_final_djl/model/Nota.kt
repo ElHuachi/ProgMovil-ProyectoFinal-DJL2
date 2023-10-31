23475aece81c58ae7a8448a861043aeb41897ff6
Manuel <perezmane07@gmail.com>
2023-10-30 22:28:51 -0600

    Actualización de la data class Nota para que actue como tabla para nuestra base de datos.

--- Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/model/Nota.kt
+++ Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/model/Nota.kt
@@ -1,11 +1,13 @@
 package com.daviddj.proyecto_final_djl.model
 
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 import java.util.Date
 data class Nota(
-    var id: Int,
-    var name: String,
-    var description: String,
-    val fecha: LocalDateTime,
-    var contenido: String
+    @PrimaryKey (autoGenerate = true) var id: Int,
+    @ColumnInfo(name = "name") var name: String,
+    @ColumnInfo(name = "description") var description: String,
+    @ColumnInfo(name = "fecha") val fecha: LocalDateTime,
+    @ColumnInfo(name = "contenido") var contenido: String
 )
