52c138e397817bf5004086bbf0c38297fc2e960a
Manuel <perezmane07@gmail.com>
2023-10-30 22:31:02 -0600

    Actualización de la data class Tarea para que actue como tabla para nuestra base de datos.

--- Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/model/Tarea.kt
+++ Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/model/Tarea.kt
@@ -1,12 +1,14 @@
 package com.daviddj.proyecto_final_djl.model
 
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 import java.util.Date
 data class Tarea (
-    var id: Int,
-    var name: String,
-    var description: String,
-    val fecha: LocalDateTime,
-    var isComplete: Boolean,
-    var contenido: String
+    @PrimaryKey(autoGenerate = true) var id: Int,
+    @ColumnInfo(name = "name") var name: String,
+    @ColumnInfo(name = "description") var description: String,
+    @ColumnInfo(name = "fecha") val fecha: LocalDateTime,
+    @ColumnInfo(name = "isComplete") var isComplete: Boolean,
+    @ColumnInfo(name = "contenido") var contenido: String
 )
