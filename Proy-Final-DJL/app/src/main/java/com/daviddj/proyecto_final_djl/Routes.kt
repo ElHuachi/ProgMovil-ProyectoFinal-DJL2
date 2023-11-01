171103c08c02c2ea969173f83de55aa90a9a3f2d
Juan José Arreola Gonzalez <juanjosearreola99@gmail.com>
2023-10-31 20:28:09 -0600

    Delete

--- Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/Routes.kt
+++
@@ -1,9 +0,0 @@
-package com.daviddj.proyecto_final_djl
-
-sealed class Routes(val route: String){
-    object NotasEditor: Routes("notasEditor")
-    object NotasScreen: Routes("notasScreen")
-    object TareasEditor: Routes("tareasEditor")
-    object TareasScreen: Routes("tareasScreen")
-}
-
