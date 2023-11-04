b3afcc5ffe005cf15c5f101ec9cb2b05462f522e
David09 <s20120178@alumnos.itsur.edu.mx>
2023-11-03 22:35:25 -0600

    Correcciones ViewModel Tareas

--- Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/viewModel/TareasEditorViewModel.kt
+++ Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/viewModel/TareasEditorViewModel.kt
@@ -5,12 +5,17 @@ import androidx.annotation.RequiresApi
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.daviddj.proyecto_final_djl.model.NotasInfo
 import com.daviddj.proyecto_final_djl.model.Tarea
+import com.daviddj.proyecto_final_djl.model.TareasInfo
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 
+@RequiresApi(Build.VERSION_CODES.O)
 class TareasEditorViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(AppUiState())
     val uiState: StateFlow<AppUiState> = _uiState.asStateFlow()
@@ -20,6 +25,12 @@ class TareasEditorViewModel: ViewModel() {
     @RequiresApi(Build.VERSION_CODES.O)
     val fecha = mutableStateOf(LocalDateTime.now())
     var tareaCargada = false
+
+    init {
+        viewModelScope.launch {
+            loadTarea(0, TareasInfo.tareas)
+        }
+    }
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun loadTarea(id: Int, tareas: List<Tarea>) {
