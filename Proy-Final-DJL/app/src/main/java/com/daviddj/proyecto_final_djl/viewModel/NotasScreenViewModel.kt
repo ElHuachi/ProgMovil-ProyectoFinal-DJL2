c4f30750aa8ac02e2edc5c77938e5e79428a1835
Manuel <perezmane07@gmail.com>
2023-11-18 00:25:10 -0600

    Actualización de la clase NotasScreenViewModel, en la cual se agrego una función para poder mostrar la lista de las notas de la base de datos.

--- Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/viewModel/NotasScreenViewModel.kt
+++ Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/viewModel/NotasScreenViewModel.kt
@@ -5,11 +5,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.daviddj.proyecto_final_djl.data.NotasRepository
+import com.daviddj.proyecto_final_djl.model.Nota
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
-class NotasScreenViewModel: ViewModel()  {
+class NotasScreenViewModel(itemsRepository: NotasRepository): ViewModel()  {
     private val _uiState = MutableStateFlow(AppUiState())
     val uiState: StateFlow<AppUiState> = _uiState.asStateFlow()
 
@@ -26,4 +32,19 @@ class NotasScreenViewModel: ViewModel()  {
     }
 
     val busquedaInput = mutableStateOf("")
+
+    val notaUiState: StateFlow<HomeUiState> =
+        itemsRepository.getAllItemsStream().map { HomeUiState(it) }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = HomeUiState()
+            )
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
 }
+
+data class HomeUiState(val itemList: List<Nota> = listOf())
