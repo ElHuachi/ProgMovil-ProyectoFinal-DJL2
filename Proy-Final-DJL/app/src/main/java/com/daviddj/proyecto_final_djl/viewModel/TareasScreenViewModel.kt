f31d8aa1caeaada3ac9347e74abe1792adf05a16
Manuel <perezmane07@gmail.com>
2023-11-18 00:44:56 -0600

    Actualización de la clasr TareasScreenViewModel para que se puedan observar las tareas existentes de la base de datos.

--- Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/viewModel/TareasScreenViewModel.kt
+++ Proy-Final-DJL/app/src/main/java/com/daviddj/proyecto_final_djl/viewModel/TareasScreenViewModel.kt
@@ -3,13 +3,33 @@ package com.daviddj.proyecto_final_djl.viewModel
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.daviddj.proyecto_final_djl.data.TareasRepository
+import com.daviddj.proyecto_final_djl.model.Tarea
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
-class TareasScreenViewModel: ViewModel() {
+class TareasScreenViewModel(itemsRepository: TareasRepository): ViewModel() {
     private val _uiState = MutableStateFlow(AppUiState())
     val uiState: StateFlow<AppUiState> = _uiState.asStateFlow()
 
     val busquedaInput = mutableStateOf("")
+
+    val tareaUiState: StateFlow<TaskUiState> =
+        itemsRepository.getAllItemsStream().map { TaskUiState(it) }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = TaskUiState()
+            )
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
+
+data class TaskUiState(val itemList: List<Tarea> = listOf())
